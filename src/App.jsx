e23a7602f098177d977b464c5238aafa119d4168
Supaluck Singjan <supalucksingjan@gmail.com>
2025-05-26 00:31:13 +0700

    Add tailwind

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+
+  
+
+
+  return (
+    <div className='grid grid-cols-3 gap-4 mt-4'>
+      <div class="col-start-2">
+        <button 
+          className='border-zinc-500 rounded-md bg-blue-100 p-3'
+          
+          >
+          Show Cat Detail
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
