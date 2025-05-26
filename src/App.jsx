308822612600f94cc66aa18591ed4e7f08ff3bd6
Supaluck Singjan <supalucksingjan@gmail.com>
2025-05-26 23:33:35 +0700

    map data

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,31 @@
-import { useState, useEffect} from "react";
+//import { useState, useEffect} from "react";
 import Axios from "axios";
 import './App.css';
 
 function App() {
 
-  const [catFact, setCatFact] = useState("");
+  const getCat = () => {
+    Axios.get('https://catfact.ninja/breeds')
+    .then(res => {
 
-  const showCatFact = () => {
-    Axios.get("https://catfact.ninja/fact")
-     .then((res) => {
-      setCatFact(res.data.fact)
-     })
+      const arrData = res.data.data.map((arr) => ({
+        ...arr
+      }))
+
+      console.log(arrData)
+
+      console.log(arrData)
+      }).catch(err => {
+        console.log(err)
+      })
   }
-
-  useEffect(() => {
-    Axios.get("https://catfact.ninja/fact")
-     .then((res) => {
-      setCatFact(res.data.fact)
-     })
-  }, []);
+  
 
   return (
     <div className='grid grid-cols-3 gap-4 mt-4'>
       <div class="col-start-2">
-        <button 
-          className='border-zinc-500 rounded-md bg-blue-100 p-3'
-          onClick={showCatFact}
-          >
-          Show Cat Detail
-        </button>
-
-        <div>
-
-          {catFact}
-        </div>
+        <button className="bg-red-100 p-3"
+        onClick={getCat}>Get Quote</button>
       </div>
     </div>
   )
